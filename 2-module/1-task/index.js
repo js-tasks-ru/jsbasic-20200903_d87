e5ee88745e8717da8774328c4f937af0f7452fa6
Daria Vikhlyantseva <d87@bk.ru>
2020-09-14 00:38:56 +0700

    Решение задач 2 модуля №1 и №2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,27 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+// function sumSalary(salaries) {
+//   let sum;
+//   for (let key in salaries) {
+//     for (let key in salaries) {
+//       if ((typeof salaries[key]) === `number`) {
+//         sum += salaries[key];
+//       }
+//     }
+//     return sum;
+//   }
+//   return 0;
+// }
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if ((typeof salaries[key]) === `number`) {
+      sum = sum + salaries[key];
+    }
+
+  }
+  return sum;
+
 }
